6ea134504839f85944966fffc1996b2cb06b2427
lokesh <lokesh15mca@gmail.com>
2022-01-20 01:26:40 +0530

    deployed all project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -20,7 +20,7 @@ function Project() {
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                <a target="_blank" rel = "noopener noreferrer" href="https://capdas-suvamadhikary.vercel.app/signup">
+                <a target="_blank" rel = "noopener noreferrer" href="https://capdas-suvamadhikary.vercel.app">
                     <div class="card-flyer">
                         <div class="text-box">
                             <div class="image-box">
@@ -30,7 +30,7 @@ function Project() {
                                 <h6>Capdas-Ecommerce</h6>
                                 <p>Capdas is our self designed and developed mobile website which provides sustainable clothing store providing platform for national seller and tailors to provide their services online.</p>
                                 <div className='buttonAncher'>
-                                <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="https://github.com/suvamAdhikary/Capdas-ECommerce" className="project__btn">View Details</a>
+                                <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="https://capdas-suvamadhikary.vercel.app" className="project__btn">View Details</a>
                                 </div>
                             </div>
                             
@@ -69,7 +69,7 @@ function Project() {
                                 <h6>Clone of optimizely</h6>
                                <p>Optimizely is an American company that provides A/B testing and multivariate testing tools, website personalization, and feature toggle capabilities, as well as web content management and digital commerce.</p>
                                <div className='buttonAncher'>
-                            <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="https://github.com/vinayak-pk/Optimizely-clone" className="project__btn">View Details</a>
+                            <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="https://op-clone.vercel.app/" className="project__btn">View Details</a>
                             </div>
                             </div>
                             
