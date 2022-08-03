854f5cbb3617337a51fc9f4555c353e5588a0b57
lokesh <lokesh15mca@gmail.com>
2022-08-04 03:19:00 +0530

    Adding new projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,6 +5,9 @@ import Project1 from '../Img/project1.png'
 import Project2 from '../Img/project2.png'
 // import Project4 from '../Img/project2.1.png'
 import opt from '../Img/opt-page.png'
+import Alriyadah from '../Img/Alriyadah1.PNG'
+import Addroitte from '../Img/Addroitte.jpg'
+import Addroitteweb from '../Img/Addroitteweb.jpg'
 
 function Project() {
   return (
@@ -15,6 +18,88 @@ function Project() {
         Work speaks louder than words.
         </p>
       </div>
+
+    {/* Company project  */}
+
+    <div id="cards_landscape_wrap-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <a target="_blank" rel = "noopener noreferrer" href="">
+                    <div class="card-flyer">
+                        <div class="text-box">
+                            <div class="image-box">
+                                <img src={Alriyadah} alt="" />
+                            </div>
+                            <div class="text-container">
+                                <h6>Alriyadah</h6>
+                                <p>Alriyadah is a driving education. The principles of traffic safety by raising
+the quality and excellence standards in providing the best services related
+to driving schools throughout the Kingdom of Saudi Arabia and in
+accordance with the highest international standards,</p>
+                                <div className='buttonAncher'>
+                                <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="" className="project__btn">View Details</a>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <a target="_blank" rel = "noopener noreferrer" href="https://cloning-premierleague.netlify.app/">
+                    <div class="card-flyer">
+                        <div class="text-box">
+                            <div class="image-box">
+                                <img src={Addroitteweb} alt="" />
+                            </div>
+                            <div class="text-container">                                    
+                                <h6>Addroitte web</h6>
+                                <p>Addroitte is a portal where student can search the internship and institue
+post the course that will help students to find the intern and company
+will post the intership and job on same.
+website for the Institute and Company to post the courses and Internship or job.
+</p>
+                                <div className='buttonAncher'>
+                                <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="https://cloning-premierleague.netlify.app/" className="project__btn">View Details</a>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <a target="_blank" rel = "noopener noreferrer" href="https://op-clone.vercel.app/">
+                    <div class="card-flyer">
+                        <div class="text-box">
+                            <div class="image-box">
+                                <img src={Addroitte} alt="" />
+                            </div>
+
+                            <div class="text-container">
+                                <h6>Addroitte Mobile App</h6>
+                               <p>Addroitte is a portal where student can search the internship and institue
+post the course that will help students to find the intern and company
+will post the intership and job on same.
+Mobile App for students to access in mobile and apply for courses and internship.
+</p>
+                               <div className='buttonAncher'>
+                            <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="https://op-clone.vercel.app/" className="project__btn">View Details</a>
+                            </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+    {/* Company project end */}
+
+
 
       <div id="cards_landscape_wrap-2">
     <div class="container">
