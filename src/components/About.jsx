73cd45ab6615c7c9268e61eafa67f633c684ce0b
Lokesh  singh <40353883+lokesh15mca@users.noreply.github.com>
2024-11-21 21:47:42 +0530

    Update 21-11-2024 About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,11 +13,10 @@ function About() {
           <div className="col__2">
             <h1 className="about__heading">About Me</h1>
             <div className="about__meta">
-              <p className="about__text p__color">I am a Full-stack developer with a deep knowledge of JavaScript, HTML, CSS, and React and a 1000 hours of coding practice. Looking at joining a challenging work environment where I can develop a deeper understanding of web development.
-              </p>
+              <p className="about__text p__color">I am a dedicated Full-Stack Developer with 6 years of experience and extensive expertise in JavaScript, HTML, CSS, Node.js, Express.js, and React. I am eager to contribute to a dynamic and challenging work environment that fosters continuous growth and allows me to deepen my expertise in web development.</p>
               
               <div className="about__button d__flex align__items__center">
-                <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="https://drive.google.com/file/d/1lG0lKTzaT4UExsx7KpbFIREo25ltLrHs/view?usp=sharing">
+                <a style={{display: "table-cell"}} target="_blank" rel = "noopener noreferrer" href="https://drive.google.com/file/d/1wMZIDzelRma-jJRYMGeAkkM0epBczH9A/view?usp=sharing">
                   <button className="about btn pointer">Resume</button>
                 </a>
               </div>
